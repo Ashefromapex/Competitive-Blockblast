7a9ce9dcfbb7410b203e49185f5714689c795579
luca <ashefromapex@proton.me>
2025-05-13 10:21:46 +0200

    changed structure

--- src/com/blockblast/storage/data.java
+++ src/com/blockblast/storage/data.java
@@ -1,6 +1,7 @@
 package com.blockblast.storage;
 
 public class data {
+    //this class is used to store data and read it again at startup
     public data(){
         // Constructor code here
     }
