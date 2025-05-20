68fb3cb275af0bc43baa4a5af0873be0ec0c689e
luschaar <luzia@schaarschmidt.bayern>
2025-05-20 11:04:18 +0200

    blöcke hinzugefügt

--- src/com/blockblast/gui/gui.java
+++ src/com/blockblast/gui/gui.java
@@ -5,17 +5,17 @@ public class gui extends JFrame {
 
 
     block block1einser;
-    block block2linie3;
-    block block3linie4;
-    block block4linie5;
-    block block5linie2;
+    block block2linie2;
+    block block3linie3;
+    block block4linie4;
+    block block5linie5;
     block block6ecke;
     block block72x2;
     block block83x3;
     block block9LBlock;
     block block10squiggles;
-    block block11großeecke;
-    block block12T;
+    block block11grosseecke;
+    block block12TBlock;
     block block132x3;
 
 
@@ -30,11 +30,32 @@ public class gui extends JFrame {
     }
     public void initWindow()
     {
+       //Luzia: instanzieren
+        block1einser = new block();
+        block2linie2 = new block();
+        block3linie3 = new block();
+        block4linie4 = new block();
+        block5linie5 = new block();
+        block6ecke = new block();
+        block72x2 = new block();
+        block83x3 = new block();
+        block9LBlock = new block();
+        block10squiggles = new block();
+        block11grosseecke = new block();
+        block12TBlock = new block();
+        block132x3 = new block();
+        // positionen festlegen, erstmal einer
+        //block1einser.setBounds(5,10,4,3);
+        this.getContentPane().add(block1einser);
 
+        this.pack();
     }
 
 }
 class block
 {
+  public block()
+  {
 
+  }
 }
