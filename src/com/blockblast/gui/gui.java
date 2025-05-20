ff964cf9b920ee5243f46c7caf0c357b49e28e81
luschaar <luzia@schaarschmidt.bayern>
2025-05-20 10:46:21 +0200

    blöcke hinzugefügt

--- src/com/blockblast/gui/gui.java
+++ src/com/blockblast/gui/gui.java
@@ -1,14 +1,40 @@
 package com.blockblast.gui;
+import javax.swing.JFrame;
 
-public class gui {
+public class gui extends JFrame {
 
-    public gui() {
 
+    block block1einser;
+    block block2linie3;
+    block block3linie4;
+    block block4linie5;
+    block block5linie2;
+    block block6ecke;
+    block block72x2;
+    block block83x3;
+    block block9LBlock;
+    block block10squiggles;
+    block block11großeecke;
+    block block12T;
+    block block132x3;
+
+
+    public gui()
+    {
+       this.getContentPane().setLayout(null);
+       this.initWindow();
     }
     public void test()
     {
         System.out.println("Hello and welcome fron gui!");
     }
+    public void initWindow()
+    {
 
-    //test
+    }
+
+}
+class block
+{
+
 }
