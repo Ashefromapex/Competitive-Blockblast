4a664c14bb1b2b9ed89749876c0dd35319975d8c
maschmel <webmagy@gmail.com>
2025-05-21 12:14:36 +0200

    Window

---
+++ src/com/blockblast/gui/window/JFrame.java
@@ -0,0 +1,4 @@
+package com.blockblast.gui.window;
+
+public class JFrame {
+}
---
+++ src/com/blockblast/gui/window/Window.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Window extends JFrame
+{
+    public Window()
+    {
+        setTitle("Window");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+    }
+
+}
