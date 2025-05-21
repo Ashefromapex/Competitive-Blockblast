bd14ca41ab1bb04e52291912743993a7d9248b30
Mullis <maurice.l.wirth@gmail.com>
2025-05-21 19:52:14 +0200

    Board class implemented methods: -check_column -check_row -reset_column -reset_row -check_field and changed the board matrix from boolean to int

--- src/com/blockblast/logic/Board.java
+++ src/com/blockblast/logic/Board.java
@@ -4,18 +4,83 @@ package com.blockblast.logic;
 
 public class Board
 {
-    boolean[][] board;
+    int[][] board;
     public Board()
     {
 
-        board = new boolean[8][8];
+        board = new int[8][8];
         for(int x = 0; x < 8; x++) //set all values in matrix to false
         {
             for(int y = 0; y < 8; y++)
             {
-                board[x][y] = false;
+                board[x][y] = 0;//x is row and y is column
             }
         }
     }
 
+    public void check_field(int[][] board)//score ?
+    {
+        for (int x = 0; x < 8; x++)
+        {
+            check_column(x);
+            check_row(x);
+        }
+    }
+
+
+
+
+
+
+
+
+
+    public void check_column(int columnY)
+    {
+        int check = 0;
+        for(int x = 0; x < 8;)//check if column Y is full
+        {
+            check = check + board[x][columnY];
+        }
+        if(check == 8)//check is only 8 if all einträge = 1 aka the column is full and needs to be cleared
+        {
+            reset_column(columnY);//resets the column
+        }
+    }
+
+    public void check_row(int rowX)
+    {
+        int check = 0;
+        for(int y = 0; y < 8;)//check if row X is full
+        {
+            check = check + board[rowX][y];
+        }
+        if(check == 8)
+        {
+            reset_row(rowX);
+        }
+    }
+
+    private void reset_column(int columnY)
+    {
+        for (int y = 0; y < 8; y++)
+        {
+            board[y][columnY] = 0;
+        }
+    }
+
+    private void reset_row(int rowX)
+    {
+        for (int y = 0; y < 8; y++)
+        {
+            board[rowX][y] = 0;
+        }
+    }
+
+
+
 }
+
+
+
+
