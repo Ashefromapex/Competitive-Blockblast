cbb89be55e0e1362c62efeaaf29673ce00c1ac9c
Mullis <maurice.l.wirth@gmail.com>
2025-05-21 18:29:43 +0200

    in algo board erstellen in boeard 8x8 feld erstellen und false setten

--- src/com/blockblast/logic/algo.java
+++ src/com/blockblast/logic/algo.java
@@ -1,8 +1,10 @@
 package com.blockblast.logic;
 
-public class algo {
-
-    public algo() {
-
+public class algo
+{
+    board board;
+    public algo()
+    {
+        board = new board();
     }
 }
--- src/com/blockblast/logic/board.java
+++ src/com/blockblast/logic/board.java
@@ -1,8 +1,21 @@
 package com.blockblast.logic;
 
+//import java.lang.reflect.Array;
+
 public class board
 {
+    boolean[][] board;
+    public board()
+    {
 
-
+        board = new boolean[8][8];
+        for(int x = 0; x < 8; x++) //set all values in matrix to false
+        {
+            for(int y = 0; y < 8; y++)
+            {
+                board[x][y] = false;
+            }
+        }
+    }
 
 }
