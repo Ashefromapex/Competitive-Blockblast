ece4a6722898d3be226106c942be0a6adb36e412
Kinder <luzia@schaarschmidt.bayern>
2025-05-22 15:11:29 +0200

    hab versucht das er die blöcke visualisiert keine ahnung ob das funktioniert errrors hats keine

--- src/com/blockblast/gui/Gui.java
+++ src/com/blockblast/gui/Gui.java
@@ -1,48 +1,30 @@
 package com.blockblast.gui;
-import javax.swing.JFrame;
-public class Gui extends JFrame {
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.Graphics;
+public class Gui extends JPanel {
 
 
-    BlockGui block1einser;
-    BlockGui block2linie2;
-    BlockGui block3linie3;
-    BlockGui block4linie4;
-    BlockGui block5linie5;
-    BlockGui block6ecke;
-    BlockGui block72x2;
-    BlockGui block83x3;
-    BlockGui block9LBlock;
-    BlockGui block10squiggles;
-    BlockGui block11grosseecke;
-    BlockGui block12TBlock;
-    BlockGui block132x3;
+    private JPanel[] panel;
+    private  BlockGui block;
 
+    public Gui() {
 
-    public Gui()
-    {
-       this.getContentPane().setLayout(null);
-       this.initWindow();
     }
-    public void test()
+    // die methode sollte eigentlich den block visualisieren
+    public void blockVisualisieren(int Blockart, char Rotation, Graphics g)
     {
-        System.out.println("Hello and welcome fron gui!");
-    }
-    public void initWindow()
-    {
-       //Luzia: instanzieren
-        block1einser = new BlockGui();
-        block2linie2 = new BlockGui();
-        block3linie3 = new BlockGui();
-        block4linie4 = new BlockGui();
-        block5linie5 = new BlockGui();
-        block6ecke = new BlockGui();
-        block72x2 = new BlockGui();
-        block83x3 = new BlockGui();
-        block9LBlock = new BlockGui();
-        block10squiggles = new BlockGui();
-        block11grosseecke = new BlockGui();
-        block12TBlock = new BlockGui();
-        block132x3 = new BlockGui();
+        int[][] array = block.blockErstellen(Blockart,Rotation);
+        for( int i = 0; i < array.length; i++ ) {
+            for( int j = 0; j < array[0].length; j++ ) {
+                if( array[i][j] == 0 ) g.setColor( Color.red );
+                else g.setColor( Color.black );
+                g.drawRect( 10*i, 10*j, 10, 10 );
+            }
+        }
+
+
+
     }
 
 }
@@ -55,37 +37,57 @@ class BlockGui extends Gui
   {
 
   }
-  private BlockGui blockErstellen(int Blockart, char Rotation)
+  public int [][] blockErstellen(int Blockart, char Rotation) // erstellen von jedem block 1-10 in einem Array
   {
       int[][] Blockarray= new int[5][5];
       switch (Blockart)
       {
         case 1://Block 1 der einser
         {
-          /*Blockarray[0] = {0,0,0,0,0};
-          Blockarray[1] = {0,0,0,0,0};
-          Blockarray[2] = {0,0,1,0,0};
-          Blockarray[3] = {0,0,0,0,0};
-          Blockarray[4] = {0,0,0,0,0};*/
+          Blockarray[0] = new int[]{0, 0, 0, 0, 0};
+          Blockarray[1] = new int[]{0, 0 ,0 ,0 ,0};
+          Blockarray[2] = new int[]{0, 0, 1, 0, 0};
+          Blockarray[3] = new int[]{0, 0, 0, 0, 0};
+          Blockarray[4] = new int[]{0, 0, 0, 0, 0};
+
+          return Blockarray;
         }
         case 2: // Block 2 Zweier Linie
         {
+            Blockarray[0] = new int[]{0, 0, 0, 0, 0};
+            Blockarray[1] = new int[]{0, 0 ,0 ,0 ,0};
+            Blockarray[2] = new int[]{0, 0, 1, 0, 0};
+            Blockarray[3] = new int[]{0, 0, 0, 0, 0};
+            Blockarray[4] = new int[]{0, 0, 0, 0, 0};
 
+            return Blockarray;
         }
         case 3: // Block 3 Dreierlinie
         {
+            Blockarray[0] = new int[]{0, 0, 0, 0, 0};
+            Blockarray[1] = new int[]{0, 0 ,0 ,0 ,0};
+            Blockarray[2] = new int[]{1, 1, 1, 0, 0};
+            Blockarray[3] = new int[]{0, 0, 0, 0, 0};
+            Blockarray[4] = new int[]{0, 0, 0, 0, 0};
 
+            return Blockarray;
         }
         case 4:// Block 4 Viererlinie
         {
+            Blockarray[0] = new int[]{0, 0, 0, 0, 0};
+            Blockarray[1] = new int[]{0, 0 ,0 ,0 ,0};
+            Blockarray[2] = new int[]{1, 1, 1, 1, 0};
+            Blockarray[3] = new int[]{0, 0, 0, 0, 0};
+            Blockarray[4] = new int[]{0, 0, 0, 0, 0};
 
+            return Blockarray;
         }
 
 
 
 
       }
-      return this;
+      return Blockarray;
   }
 
 }
