5936f278641c49dd0009219efc0e763949824bd1
Magdalena <webmagy@gmail.com>
2025-06-04 12:06:58 +0200

    Window mit Überschrift, Buttons

---
+++ src/com/blockblast/gui/window/Launcher.java
@@ -0,0 +1,15 @@
+package com.blockblast.gui.window;
+
+import javax.swing.*;
+
+public class Launcher
+{
+
+
+    public static void main(String[] args)
+    {
+        //erzeugt Controller
+        Controller controller = new Controller();
+        controller.startGui();
+    }
+}
