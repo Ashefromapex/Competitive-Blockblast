9c64b44d40b2e48e8d2a68dbf402a1a69e8ad64f
luca <ashefromapex@proton.me>
2025-06-23 17:51:27 +0200

    blockerstellen deleted

--- src/com/blockblast/gui/Gui.java
+++ src/com/blockblast/gui/Gui.java
@@ -68,59 +68,5 @@ class BlockGui extends Gui
   {
 
   }
-  public int [][] blockErstellen(int Blockart, char Rotation) // erstellen von jedem block 1-10 in einem Array
-  {
-      int[][] Blockarray= new int[5][5];
-      switch (Blockart)
-      {
-        case 1://Block 1 der einser
-        {
-          Blockarray[0] = new int[]{0, 0, 0, 0, 0};
-          Blockarray[1] = new int[]{0, 0 ,0 ,0 ,0};
-          Blockarray[2] = new int[]{0, 0, 1, 0, 0};
-          Blockarray[3] = new int[]{0, 0, 0, 0, 0};
-          Blockarray[4] = new int[]{0, 0, 0, 0, 0};
-
-          return Blockarray;
-        }
-        case 2: // Block 2 Zweier Linie
-        {
-            Blockarray[0] = new int[]{0, 0, 0, 0, 0};
-            Blockarray[1] = new int[]{0, 0 ,0 ,0 ,0};
-            Blockarray[2] = new int[]{0, 1, 1, 0, 0};
-            Blockarray[3] = new int[]{0, 0, 0, 0, 0};
-            Blockarray[4] = new int[]{0, 0, 0, 0, 0};
-
-            return Blockarray;
-        }
-        case 3: // Block 3 Dreierlinie
-        {
-            Blockarray[0] = new int[]{0, 0, 0, 0, 0};
-            Blockarray[1] = new int[]{0, 0 ,0 ,0 ,0};
-            Blockarray[2] = new int[]{1, 1, 1, 0, 0};
-            Blockarray[3] = new int[]{0, 0, 0, 0, 0};
-            Blockarray[4] = new int[]{0, 0, 0, 0, 0};
-
-            return Blockarray;
-        }
-        case 4:// Block 4 Viererlinie
-        {
-            Blockarray[0] = new int[]{0, 0, 0, 0, 0};
-            Blockarray[1] = new int[]{0, 0 ,0 ,0 ,0};
-            Blockarray[2] = new int[]{1, 1, 1, 1, 0};
-            Blockarray[3] = new int[]{0, 0, 0, 0, 0};
-            Blockarray[4] = new int[]{0, 0, 0, 0, 0};
-
-            return Blockarray;
-        }
-
-
-
-
-      }
-      return Blockarray;
-
-
-  }
 
 }
