c6d27f0523d07cae31fcc07cf9ba9d3c0a85c746
Laminat <liam7weinstock@gmail.com>
2025-06-26 16:14:05 +0200

    GUI for the main playing field is DONE (finally)

--- src/com/blockblast/gui/window/Window.java
+++ src/com/blockblast/gui/window/Window.java
@@ -18,18 +18,12 @@ public class Window extends JFrame {
     public Window(ActionListener listener) {
 
 
-
-
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-
-
-        setVisible(true);
-
         //Window erstellen
         setTitle("BlockBlast");
 
+
         //Button erstellen south
+        //ist zur Zeit nicht verwendet
         JPanel buttonPanel = new JPanel();
         JButton repeatButton = new JButton("Repeat");
         JButton startButton = new JButton("Start");
@@ -40,32 +34,58 @@ public class Window extends JFrame {
         JPanel mainPanel = new JPanel();
         GridLayout mainPanelGridLayout = new GridLayout(0,8);
         mainPanel.setLayout(mainPanelGridLayout);
-        int mainPanelBorder = 50;
+        mainPanel.setOpaque(true);
+        int mainPanelBorder = 50; //Abstand vom Rand des Fensters
+        int boardSize = 640; //Größe des Boards
+        mainPanel.setBounds(0,50,boardSize,boardSize); //Position und Größe des Boards im Layout
         EmptyBorder Distance = new EmptyBorder(mainPanelBorder, mainPanelBorder, mainPanelBorder, mainPanelBorder);
         LineBorder mainPanelOutline = new LineBorder(Color.black);
-        CompoundBorder mainPanelCompoundBorder = new CompoundBorder(Distance, mainPanelOutline);
+        CompoundBorder mainPanelCompoundBorder = new CompoundBorder(Distance, mainPanelOutline); //Coole schwarze Outline
         mainPanel.setBorder(mainPanelCompoundBorder);
         mainPanel.setBackground(Color.DARK_GRAY);
-        mainPanel.setPreferredSize(new Dimension(400,400));
-        mainPanel.setMaximumSize(new Dimension(400,400));
+        for(int i = 0;i < 64;i++) //Code Monstrum zusammengefasst
+        {
+            JButton b = new JButton();
+            b.setBackground(Color.GRAY);
+            mainPanel.add(b);
+            grid.add(b);
+        }
+
+        //Fake Board zum Blöcke hovern erstellen, sieht genauso aus wie das Main Board
+        JPanel fakeBoard = new JPanel();
+        GridLayout fakeBoardGridLayout = new GridLayout(0,8);
+        fakeBoard.setLayout(fakeBoardGridLayout);
+        fakeBoard.setBounds(0,50,boardSize,boardSize);
+        fakeBoard.setOpaque(false); //macht durchsichtig
+        fakeBoard.setBackground(new Color(0,0,0,0)); //macht durchsichtig
+        fakeBoard.setBorder(Distance);
+        for(int i = 0;i < 64;i++) //Füllen mit Blöcken
+        {
+            JButton b = new JButton();
+            b.setOpaque(false); //macht durchsichtig
+            b.setContentAreaFilled(false); //macht Blöcke innen durchsichtig
+            b.setBorderPainted(false); //macht Block Outlines durchsichtig
+            fakeBoard.add(b);
+        }
+
 
         //BlockPanel erstellen und Space für die einzelnen Blöcke
-        JPanel blockPanel = new JPanel();
+        JPanel blockPanel = new JPanel(); //Panel für die BlockPreviews
         blockPanel.setBackground(Color.DARK_GRAY);
-        BoxLayout blockPanelLayout = new BoxLayout(blockPanel,BoxLayout.LINE_AXIS);
+        BoxLayout blockPanelLayout = new BoxLayout(blockPanel,BoxLayout.LINE_AXIS); //horizontales BoxLayout
         blockPanel.setLayout(blockPanelLayout);
-        int blockBorder = 10;
-        int blockPreviewSize = 125;
-        JPanel block1 = new JPanel();
+        int blockBorder = 10; //Abstand vom Rand
+        int blockPreviewSize = 200; //Größe des BlockPreviews
+        JPanel block1 = new JPanel(); //1.Block
         block1.setLayout(new GridLayout(5,5));
         block1.setPreferredSize(new Dimension(blockPreviewSize, blockPreviewSize));
-        for (int i = 0; i < 25; i++)
+        for (int i = 0; i < 25; i++) //Ausfüllen mit JButtons
         {
             JButton b = new JButton();
             b.setBackground(Color.GRAY);
             block1.add(b);
         }
-        JPanel block2 = new JPanel();
+        JPanel block2 = new JPanel(); //2.Block
         block2.setLayout(new GridLayout(5,5));
         block2.setPreferredSize(new Dimension(blockPreviewSize, blockPreviewSize));
         for (int i = 0; i < 25; i++)
@@ -74,7 +94,7 @@ public class Window extends JFrame {
             b.setBackground(Color.GRAY);
             block2.add(b);
         }
-        JPanel block3 = new JPanel();
+        JPanel block3 = new JPanel(); //3.Block
         block3.setLayout(new GridLayout(5,5));
         block3.setPreferredSize(new Dimension(blockPreviewSize, blockPreviewSize));
         for (int i = 0; i < 25; i++)
@@ -83,6 +103,7 @@ public class Window extends JFrame {
             b.setBackground(Color.GRAY);
             block3.add(b);
         }
+        //Elemente des BoxLayouts, von links nach rechts
         blockPanel.add(Box.createRigidArea(new Dimension(blockBorder, 0)));
         blockPanel.add(block1);
         blockPanel.add(Box.createRigidArea(new Dimension(blockBorder, 0)));
@@ -91,32 +112,19 @@ public class Window extends JFrame {
         blockPanel.add(block3);
         blockPanel.add(Box.createRigidArea(new Dimension(blockBorder, 0)));
 
-        //Unteren Rand von den BlockPreviews entfernen
-        JPanel blockOverlord = new JPanel();
+        //Abstand von unterem Rand und BlockPreviews erstellen
+        JPanel blockOverlord = new JPanel(); //Panel, was blockPanel und Abstandspanel beinhaltet
         blockOverlord.setLayout(new BorderLayout());
-        JPanel southBumper = new JPanel();
+        JPanel southBumper = new JPanel(); //Bumper zu unterem Rand
+        int southBumperHeight = 20; //Abstand zu unterem Rand
         southBumper.setBackground(Color.DARK_GRAY);
-        southBumper.setPreferredSize(new Dimension(blockBorder*4+blockPreviewSize*3, blockBorder));
+        southBumper.setPreferredSize(new Dimension(blockBorder*4+blockPreviewSize*3, southBumperHeight));
         blockOverlord.add(southBumper, BorderLayout.SOUTH);
         blockOverlord.add(blockPanel, BorderLayout.CENTER);
-
-        JColorChooser colorChooser = new JColorChooser();
-        colorChooser.setColor(Color.GRAY);
-        colorChooser.setSize(10,10);
-        colorChooser.setBounds(50,50,50,50);
-
-        for(int i = 0;i < 64;i++) //Code Monstrum zusammengefasst
-        {
-            JButton b = new JButton();
-            b.setBackground(colorChooser.getColor());
-            this.add(b);
-            mainPanel.add(b);
-            grid.add(b);
-        }
+        blockOverlord.setBounds(0,50+boardSize,boardSize,blockPreviewSize+southBumperHeight); //Position und Größe des Panels im Layout
 
 
-        mainPanel.setVisible(true);
-        blockPanel.setVisible(true);
+
 
 
         //Titel erstellen
@@ -124,11 +132,22 @@ public class Window extends JFrame {
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
         titleLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
         titleLabel.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+        titleLabel.setBounds(0,0,boardSize,50); //Bestimmt Position und Größe des Titels
+
 
         //Top label layout
-        add(titleLabel, BorderLayout.NORTH);
-        add(mainPanel, BorderLayout.CENTER);
-        add(blockOverlord, BorderLayout.SOUTH);
+        //Start und Stop Stuff
+        //fügt alles hinzu, was zuerst hinzugefügt wird ist am weitesten oben
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false); //Window bleibt genausogroß wie wir wollen
+        setVisible(true);
+        setLayout(null);
+        setSize(new Dimension(boardSize+15,50+boardSize+mainPanelBorder+blockPreviewSize+southBumperHeight-15));
+        add(titleLabel);
+        add(fakeBoard);
+        add(mainPanel);
+        add(blockOverlord);
+
 
 
 
@@ -143,7 +162,6 @@ public class Window extends JFrame {
         p.add(label);*/
 
         //gibt ideale Größe an und centert den Frame
-        pack();
         setLocationRelativeTo(null);
 
         //verdrahtung controller mit buttons
