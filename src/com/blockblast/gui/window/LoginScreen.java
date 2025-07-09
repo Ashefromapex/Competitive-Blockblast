ea93fa563309ae8bb662e58b2ba588faedf93c38
Kinder <luzia@schaarschmidt.bayern>
2025-07-09 19:33:27 +0200

    redesigned log in

--- src/com/blockblast/gui/window/LoginScreen.java
+++ src/com/blockblast/gui/window/LoginScreen.java
@@ -9,16 +9,20 @@ public class LoginScreen extends JPanel implements KeyListener
     TextField usernameField;
     ControllerGUI c;
     String username = " ";
+    private static int BildWidth = 400;
+    private static int BildHeigth = 250;
+    private static int loginWidth = 44;
+
 
 
     public LoginScreen(ControllerGUI c)
     {
         this.c=c;
-        setSize(500,600);
+        setSize(BildWidth,BildHeigth);
         setLayout(null);
-        setBackground(Color.BLUE);
+        setBackground(Color.BLACK);
         usernameField = new TextField();
-        usernameField.setBounds(50,150,250,50);
+        usernameField.setBounds(15,150,250,50);
         usernameField.setFont(new Font("Tahoma",Font.BOLD,40));
         usernameField.setBackground(Color.LIGHT_GRAY);
         usernameField.setFocusable(true);
@@ -27,14 +31,17 @@ public class LoginScreen extends JPanel implements KeyListener
 
 
 
-        JLabel headline = new JLabel("Blockblast");
-        headline.setFont(new Font("Tahoma",Font.BOLD,50));
-        headline.setBounds(100,50,300,50);
+        JLabel headline = new JLabel("Enter Username:");
+        headline.setFont(new Font("Tahoma",Font.BOLD,40));
+        headline.setBounds(0,50,BildWidth,50);
+        headline.setForeground(Color.GREEN);
+        headline.setHorizontalAlignment(JLabel.CENTER);
+        headline.setHorizontalTextPosition(SwingConstants.CENTER);
 
         JButton loginButton = new JButton("Login");
         loginButton.setFont(new Font("Tahoma",Font.BOLD,20));
         loginButton.setBackground(Color.CYAN);
-        loginButton.setBounds(310,150,100,50);
+        loginButton.setBounds(275,150,100,50);
 //        loginButton.setFocusable(true);
 //        loginButton.requestFocusInWindow();
         loginButton.addActionListener(new ActionListener() {
